7cd621629948240a88940108b5def5492df88666
ingridwwu <itzingridwu@gmail.com>
2022-01-15 12:47:23 -0800

    bye evan

---
+++ src/mainPage/main.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function main() {
+    return (
+        <div>
+            <body>
+                <section class = "show">
+                    <header>
+                        <h2 class = "name">name of our app</h2>
+                        <div class = "toggle"></div>
+                    </header>
+
+                    <video src = "video.mp4" muted loop autoplay></video>
+
+                    <div class = "overText"></div>
+
+                    <div class = "text">
+                        <h2>Music:</h2>
+                        <h3>A Universal Language.</h3>
+                        <p>asdfjkla;sdfjaklsd;faj;sdklfjadl;skfja;sdlkfj</p> 
+                        <a href = "#">Start Mingling!</a>
+                    </div>
+                </section>
+
+                <div class = "menuBear">
+                    <ul>
+                        <li><a href= "#">Home</a></li>
+                        <li><a href= "#">News</a></li>
+                        <li><a href= "#">About</a></li>
+                        <li><a href= "#">asdf</a></li>
+                    </ul>
+                </div>
+            </body>
+        </div>
+    )
+}
+
+export default main
