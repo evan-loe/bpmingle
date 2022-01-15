0d090a0b8010c57c274526ed457034e9860fc54e
evan-loe <evan.loe@mail.utoronto.ca>
2022-01-15 18:06:09 -0500

    added queue function to spotify api

--- src/spotifyStuff/spotifyAPI.js
+++ src/spotifyStuff/spotifyAPI.js
@@ -1,3 +1,5 @@
+const spotifyBaseURL = "https://api.spotify.com/v1/me/";
+
 class spotifyAPI {
   constructor() {
     this.access_token = null;
@@ -14,6 +16,14 @@ class spotifyAPI {
             callback(access_token);
           },
         });
+        this.player.connect().then((success) => {
+          if (success) {
+            console.log("The web playback SDK is workiunnnn");
+          }
+        });
+      });
+      this.player.addListener("ready", ({ device_id }) => {
+        this.device_id = device_id;
       });
     };
   }
@@ -23,5 +33,35 @@ class spotifyAPI {
     return "BQCGi4L11D9m0FnGL6kp9TZn83yuBPMtV4n5JrKfqjLKoxz8MPt_SsBOpKQiHrFA10G1OlDGsJ7-HqnlfacKdcKda5mR34aypdy6iM678h6UewMw2874UswhRCyQcGPT8XF8IGpgv_4gnkS6phahg39ny6-EgRPtj6BHjdilCXACm_PCPgmCwME";
   }
 
-  async addToQueue() {}
+  async addToQueue(uri) {
+    fetch(`${spotifyBaseURL}/queue`, {
+      method: "POST",
+      body: JSON.stringify({
+        uri: [uri],
+        device_id: this.device_id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.access_token}`,
+      },
+    });
+  }
+
+  async play(uris = []) {
+    await fetch(
+      `https://api.spotify.com/v1/me/player/play?device_id=${this.device_id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          uris: uris,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.access_token}`,
+        },
+      }
+    );
+  }
 }
+
+export default spotifyAPI;
