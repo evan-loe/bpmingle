0aa83fe75d543256e7b7dedcd7bffaa1a25301c4
BigChen520jyp <justinkw02@gmail.com>
2022-01-15 17:31:23 -0800

    added comments to server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
+//import libraries
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");//import socket.io library
+const Server = require("socket.io").Server;//import socket.io library
 
 app.use(cors());//cors midware
 
 const server = http.createServer(app);//generate server
 
-const io = new Server(server, {
+const io = new Server(server, {//pass server created to socket.io
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3000",//to be changed
         methods: ["GET", "POST"],
     },
 });
 
-io.on("connection", (socket)=> {
+io.on("connection", (socket)=> {//detect connection-> log id
     console.log(socket.id);
 
-    socket.on("disconnect", ()=> {
-        console.log("User Disconnected", socket.id);
+    socket.on("disconnect", ()=> {//detect disconnect
+        console.log("User Has Left", socket.id);
     });
 });
 
