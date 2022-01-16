dadea40d91c0b7d0b3251836284a23f98675e591
evan-loe <evan.loe@mail.utoronto.ca>
2022-01-16 11:33:52 -0500

    imported spotify player into texts

--- src/chatBox/ChatBox.js
+++ src/chatBox/ChatBox.js
@@ -3,6 +3,8 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Texts from "../Texts";
 
+import SpotifyPlayer from "../spotifyStuff/SpotifyPlayer";
+
 const user = io.connect("http://localhost:3001"); //connect frontend to backend
 
 function ChatBox() {
@@ -21,7 +23,7 @@ function ChatBox() {
   return (
     //takes in user info compentent
     <div className="Appl">
-      {!dispalyText ? ( //shows sign in if user is not signed in yet
+      {!dispalyText && (
         <div className="joinChatContainer">
           <h1>Ready to mingle?</h1>
           <input
@@ -40,9 +42,12 @@ function ChatBox() {
           />
           <button onClick={joinroom}>Start Vibing</button>
         </div>
-      ) : (
-        //display chat after sign in
-        <Texts user={user} username={username} chatroom={chatroom} />
+      )}
+      {dispalyText && (
+        <div className="spotifyAndText">
+          <SpotifyPlayer></SpotifyPlayer>
+          <Texts user={user} username={username} chatroom={chatroom} />
+        </div>
       )}
     </div>
   );
