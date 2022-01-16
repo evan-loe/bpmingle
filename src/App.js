304881cefe95b108f4c3822d3c2513f127266aa0
ingridwwu <itzingridwu@gmail.com>
2022-01-15 18:20:50 -0800

    hey guys sorry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
 import Main from "./mainPage/Main";
 import SpotifyPlayer from "./spotifyStuff/SpotifyPlayer";
+import chatBox from "./chatBox/chatBox";
 
 function App() {
   return (
@@ -10,10 +10,11 @@ function App() {
         file to add elements on this page
       </div>
       {/* TODO: remove this header thing and add our main page component here. We also probably need to decide what states needs to be lifted up to this level*/}
-    
+      <SpotifyPlayer></SpotifyPlayer>
       <div className="mainPage">
         <Main></Main>
       </div>
+      <div className="chatBox">{chatBox()}</div>
     </div>
   );
 }
--- src/mainPage/main.js
+++ src/mainPage/main.js
@@ -1,36 +1,38 @@
 import React from 'react'
+import myVideo from "./vid.mp4"
+import styles from "./Main.css"
 
 function Main() {
     return (
         <div>
-            <body>
-                <section className = "show">
-                    <header>
-                        <h2 className = "name">name of our app</h2>
-                        <div className = "toggle"></div>
-                    </header>
+            <div className = "show">
+                <header>
+                    <h2 className = "name">Travel</h2>
+                    <div className = "toggle"></div>
+                </header>
 
-                    <video src={require('./video.mp4')} muted loop autoplay></video>
+                <video muted loop autoplay>
+                    <source src = {myVideo} type = "video/mp4"/>
+                </video> 
 
-                    <div className = "overText"></div>
+                <div className = "overText"></div>
 
-                    <div className = "text">
-                        <h2>Music:</h2>
-                        <h3>A Universal Language.</h3>
-                        <p>asdfjkla;sdfjaklsd;faj;sdklfjadl;skfja;sdlkfj</p> 
-                        <a href = "#">Start Mingling!</a>
-                    </div>
-                </section>
-
-                <div className = "menuBear">
-                    <ul>
-                        <li><a href= "#">Home</a></li>
-                        <li><a href= "#">News</a></li>
-                        <li><a href= "#">About</a></li>
-                        <li><a href= "#">asdf</a></li>
-                    </ul>
+                <div className = "text">
+                    <h2>Music:</h2>
+                    <h3>A Universal Language.</h3>
+                    <p>asdfjkla;sdfjaklsd;faj;sdklfjadl;skfja;sdlkfj</p> 
+                    <a href = "#">Start Mingling!</a>
                 </div>
-            </body>
+            </div>
+
+            <div className = "menu">
+                <ul>
+                    <li><a href= "#">Home</a></li>
+                    <li><a href= "#">News</a></li>
+                    <li><a href= "#">About</a></li>
+                    <li><a href= "#">asdf</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
