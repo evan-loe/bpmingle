539027e284f92e448592e254aed68a49710c19c1
evan-loe <evan.loe@mail.utoronto.ca>
2022-01-17 01:44:53 -0500

    tried changing url to fix cors

--- client/src/chatBox/ChatBox.js
+++ client/src/chatBox/ChatBox.js
@@ -6,9 +6,9 @@ import Texts from "../Texts";
 import SpotifyPlayer from "../spotifyStuff/SpotifyPlayer";
 
 const user = io.connect(
-  process.env.PRODUCTION_ENV === "PRODUCTION"
-    ? `https://bpmingle.herokuapp.com/${process.env.PORT || 3001}`
-    : "http://localhost:3001/api/callback"
+  process.env.PRODUCTION_ENV === "DEVELOPMENT"
+    ? "http://localhost:3001/api/callback"
+    : `https://bpmingle.herokuapp.com/${process.env.PORT || 3001}`
 ); //connect frontend to backend
 
 function ChatBox(prop) {
